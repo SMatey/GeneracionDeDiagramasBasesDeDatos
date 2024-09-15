51893a564873fcb1be268a249f5e33e4763a14b4
SMatey <casamarosebas@gmail.com>
2024-09-15 14:36:43 -0600

    Front

--- diagram-generator/src/Body.jsx
+++ diagram-generator/src/Body.jsx
@@ -3,18 +3,62 @@ import PropTypes from 'prop-types';
 
 export const Body = () => {
     //=============REQUIRED FUNCTIONS=================
+
+
+    //=============HTML BODY=================
+    //<!--Cambiar los placesHolder-->
     return (
-        <>
-            <body>
-                <form className='formulario'>
-                    <h2>Generador de diagramas</h2>
-                    <input className='form_input' type='text' placeholder='Host'></input>
-                    <input className='form_input' type='number' placeholder='Port'></input>
-                    <input className='form_input' type='text' placeholder='User Name'></input>
-                    <input className='form_input' type='text' placeholder='Password'></input>
-                    <input className='form_input' type='text' placeholder='DB Name'></input>
+    <>
+        <div className="container">
+            <div className='BDData'>
+                <h2>Generador de diagramas</h2>
+                <form>
+                    <div>
+                        <label htmlFor="host">Host</label>
+                        <input className='form_input' type='text' placeholder='Host' id="host"></input>
+                    </div>
+                    <div>
+                        <label htmlFor="port">Port</label>
+                        <input className='form_input' type='number' placeholder='Port' id="port"></input>
+                    </div>
+                    <div>
+                        <label htmlFor="user">User Name</label>
+                        <input className='form_input' type='text' placeholder='User Name' id="user"></input>
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password</label>
+                        <input className='form_input' type='password' placeholder='Password' id="password"></input>
+                    </div>
+                    <div>
+                        <label htmlFor="dbName">DB Name</label>
+                        <input className='form_input' type='text' placeholder='DB Name' id="dbName"></input>
+                    </div>
+
+                    <div className="form_input">
+                        <label>Tipo de Base de Datos
+                            <select className="type-of-db">
+                                <option value="MySQL">MySQL</option>
+                                <option value="Postgres">PostgreSQL</option>
+                                <option value="SQLServer">SQL Server</option>
+                            </select>
+                        </label>
+                    </div>
+
+                    <div className='form_input' id='update'>
+                        <button type='submit'>
+                            Actualizar
+                        </button>
+                    </div>
                 </form>
-            </body>
-        </>
+            </div>
+
+            <div className='DiagramGenerate'>
+                <h2>Diagrama Generado</h2>
+                <div className='diagram-placeholder'>
+                    {/* Aquí se mostrará el diagrama */}
+                </div>
+            </div>
+        </div>
+    </>
     )
 }
