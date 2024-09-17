c4b183b8dbf6010e68f53bbbacab47631d1b5524
Geancarlo <101891752+Geancaov@users.noreply.github.com>
2024-09-17 15:55:05 -0600

    Add files via upload

--- diagram-generator/vite.config.js
+++ diagram-generator/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/generar_diagrama': 'http://localhost:5000',  // Redirige las solicitudes a Flask en el puerto 5000
+    }
+  }
 })
