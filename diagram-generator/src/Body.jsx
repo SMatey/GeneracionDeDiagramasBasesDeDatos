490b0d3f9828a726172688aa91c5f24c58c8ef5e
SMatey <casamarosebas@gmail.com>
2024-09-17 19:32:13 -0600

    Buttons
    Se agrego los botones faltantes de descarga, anterior y siguiente, se les dio estilo y se agregaron sus funciones respectivas

--- diagram-generator/src/Body.jsx
+++ diagram-generator/src/Body.jsx
@@ -2,34 +2,54 @@ import { useState } from 'react';
 import axios from 'axios';
 
 export const Body = () => {
-    const [diagramUrl, setDiagramUrl] = useState(null);  // Estado para almacenar la URL de la imagen generada
+    const [imageUrls, setImageUrls] = useState([]);
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
     const handleSubmit = async (e) => {
-        e.preventDefault();  // Prevenir el comportamiento predeterminado del formulario
+        e.preventDefault();
 
-        // Recogemos los valores de los inputs
         const formData = {
             host: document.getElementById('host').value,
-            port: parseInt(document.getElementById('port').value, 10),  // Convertir a número
+            port: parseInt(document.getElementById('port').value, 10),
             user: document.getElementById('user').value,
             password: document.getElementById('password').value,
             dbName: document.getElementById('dbName').value,
-            tipo_base_datos: document.getElementById('tipo_base_datos').value
+            tipoBaseDatos: document.getElementById('tipoBaseDatos').value
         };
-        console.log('Datos enviados:', formData);
+
         try {
-            // Realizar la solicitud POST al backend
-            const response = await axios.post('http://localhost:5000/generar_diagrama', formData, {
-                responseType: 'blob',  // Recibimos el archivo binario (imagen PNG)
+            const response = await axios.post('http://localhost:5000/generate-diagram', formData, {
+                responseType: 'blob',
             });
 
-            // Crear una URL para el blob de la imagen recibida
             const imageUrl = URL.createObjectURL(response.data);
-            setDiagramUrl(imageUrl);  // Guardar la URL de la imagen para mostrarla
+            setImageUrls((prevUrls) => [...prevUrls, imageUrl]);
+            setCurrentImageIndex(imageUrls.length);
 
         } catch (error) {
             console.error('Error al generar el diagrama:', error);
         }
+    };
+
+    const handlePreviousImage = () => {
+        if (currentImageIndex > 0) {
+            setCurrentImageIndex(currentImageIndex - 1);
+        }
+    };
+
+    const handleNextImage = () => {
+        if (currentImageIndex < imageUrls.length - 1) {
+            setCurrentImageIndex(currentImageIndex + 1);
+        }
+    };
+
+    const handleDownloadImage = () => {
+        const link = document.createElement('a');
+        link.href = imageUrls[currentImageIndex];
+        link.download = `diagram-${currentImageIndex + 1}.png`;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
     };
 
     return (
@@ -39,30 +59,30 @@ export const Body = () => {
                 <form onSubmit={handleSubmit}>
                     <div>
                         <label htmlFor="host">Host</label>
-                        <input className='form_input' type='text' placeholder='127.0.0.1' id="host"></input>
+                        <input className='form_input' type='text' placeholder='127.0.0.1' id="host" />
                     </div>
                     <div>
                         <label htmlFor="port">Port</label>
-                        <input className='form_input' type='number' placeholder='3306' id="port"></input>
+                        <input className='form_input' type='number' placeholder='3306' id="port" />
                     </div>
                     <div>
                         <label htmlFor="user">User Name</label>
-                        <input className='form_input' type='text' placeholder='User Name' id="user"></input>
+                        <input className='form_input' type='text' placeholder='User Name' id="user" />
                     </div>
                     <div>
                         <label htmlFor="password">Password</label>
-                        <input className='form_input' type='password' placeholder='Password' id="password"></input>
+                        <input className='form_input' type='password' placeholder='Password' id="password" />
                     </div>
                     <div>
                         <label htmlFor="dbName">DB Name</label>
-                        <input className='form_input' type='text' placeholder='DB Name' id="dbName"></input>
+                        <input className='form_input' type='text' placeholder='DB Name' id="dbName" />
                     </div>
 
                     <div className="form_input">
                         <label>Data Base Type
-                            <select className="type-of-db" id="tipo_base_datos">
+                            <select className="type-of-db" id="tipoBaseDatos">
                                 <option value="MySQL">MySQL</option>
-                                <option value="Postgres">Postgres</option>
+                                <option value="Postgres">PostgreSQL</option>
                                 <option value="SQLServer">SQL Server</option>
                             </select>
                         </label>
@@ -73,19 +93,36 @@ export const Body = () => {
                             Update
                         </button>
                     </div>
+                    {imageUrls.length > 0 && (
+                        <button type="button" onClick={handleDownloadImage}>
+                            Descargar Imagen Actual
+                        </button>
+                    )}
                 </form>
             </div>
 
             <div className='DiagramGenerate'>
                 <h2>Diagrama Generado</h2>
                 <div className='diagram-placeholder'>
-                    {diagramUrl ? (
-                        <img src={diagramUrl} alt="Diagrama Generado" style={{ width: '600px', height: 'auto' }} />
+                    {imageUrls.length > 0 ? (
+                        <>
+                            {/* Mostrar la imagen actual */}
+                            <img src={imageUrls[currentImageIndex]} alt="Generated diagram" />
+                        </>
                     ) : (
-                        <p>No se generó ningún diagrama aún.</p>
+                        <p>No se ha generado ningún diagrama aún.</p>
                     )}
+                     {/* Botones de navegación */}
+                    <div className='nav-buttons'>
+                        <button onClick={handlePreviousImage} disabled={currentImageIndex === 0}>
+                            ⬅️ Previous
+                        </button>
+                        <button onClick={handleNextImage} disabled={currentImageIndex === imageUrls.length - 1}>
+                            Next ➡️
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
     );
-}
+};
